898719800b6e2bcf57f4951f8cf4f66f24c28bfb
wata-tsugarian <wata.tsugarian@gmail.com>
2026-02-07 21:54:35 +0900

    rubocop修正

--- db/migrate/20260207070205_create_users.rb
+++ db/migrate/20260207070205_create_users.rb
@@ -10,6 +10,6 @@ class CreateUsers < ActiveRecord::Migration[8.1]
       t.timestamps
     end
 
-    add_index :users, [:provider, :uid], unique: true
+    add_index :users, [ :provider, :uid ], unique: true
   end
 end
