1fc2089eab87190abafde894832624f0bf921920
wata-tsugarian <wata.tsugarian@gmail.com>
2026-02-08 00:07:22 +0900

    flash、ログインユーザー管理

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,24 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  inertia_share do
+    {
+      currentUser: current_user,
+      flash: {
+        notice: flash[:notice],
+        alert: flash[:alert]
+      }
+    }
+  end
+
+  private
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def authenticate_user!
+    redirect_to root_path unless current_user
+  end
 end
