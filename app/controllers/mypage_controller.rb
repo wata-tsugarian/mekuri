fbcd6ea7e071e7bd0e2bf6d850237a3d35a8861b
wata-tsugarian <wata.tsugarian@gmail.com>
2026-02-08 14:26:18 +0900

    メソッド追加

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -1,7 +1,25 @@
 class MypageController < ApplicationController
   before_action :authenticate_user!
 
-  def index
+  def show
     render inertia: "mypage/index"
+  end
+
+  def edit
+    render inertia: "mypage/edit", props: { user: current_user }
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to mypage_path, notice: "ユーザーを更新しました。"
+    else
+      redirect_back fallback_location: edit_mypage_path, inertia: { errors: current_user.errors }
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
   end
 end
